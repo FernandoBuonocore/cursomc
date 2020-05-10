e82d0acb0858eee84c2602946fe332253e8ab1ac
unknown <fernando_beckenbach@hotmail.com>
2020-05-10 17:09:44 -0300

    mudando para REST trazer categorias padrões dominio

--- src/main/java/com/buonotec/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/buonotec/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.buonotec.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.buonotec.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST FUNCIONANDO";
+	public List<Categoria> listar() {
+		
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> listaCategoria = new ArrayList<Categoria>();
+		listaCategoria.add(c1);
+		listaCategoria.add(c2);
+		
+		return listaCategoria;
 	}
 	
 }
