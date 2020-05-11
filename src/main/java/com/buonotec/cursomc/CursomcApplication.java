d31c6b551a6a2a151c42202c58505785223bd2fa
unknown <fernando_beckenbach@hotmail.com>
2020-05-10 21:28:41 -0300

    Criando operacao de instanciação

--- src/main/java/com/buonotec/cursomc/CursomcApplication.java
+++ src/main/java/com/buonotec/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.buonotec.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.buonotec.cursomc.domain.Categoria;
+import com.buonotec.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Autowired
+	private CategoriaRepository dao;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		
+		dao.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
