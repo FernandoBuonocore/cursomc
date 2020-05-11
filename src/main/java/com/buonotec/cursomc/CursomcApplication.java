3d2fe0b753362c314462a01402ec1236ffe1518f
unknown <fernando_beckenbach@hotmail.com>
2020-05-10 21:50:50 -0300

    Criando operacao de instanciação

--- src/main/java/com/buonotec/cursomc/CursomcApplication.java
+++ src/main/java/com/buonotec/cursomc/CursomcApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.buonotec.cursomc.domain.Categoria;
+import com.buonotec.cursomc.domain.Produto;
 import com.buonotec.cursomc.repositories.CategoriaRepository;
+import com.buonotec.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
@@ -18,15 +20,32 @@ public class CursomcApplication implements CommandLineRunner{
 	}
 
 	@Autowired
-	private CategoriaRepository dao;
+	private CategoriaRepository daoCategoria;
+	
+	@Autowired
+	private ProdutoRepository daoProduto;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
-		dao.saveAll(Arrays.asList(cat1, cat2));
+		cat1.getProdutos().add(p1);
+		cat1.getProdutos().add(p2);
+		cat1.getProdutos().add(p3);
+		cat2.getProdutos().add(p2);
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		daoCategoria.saveAll(Arrays.asList(cat1, cat2));
+		
+		daoProduto.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
